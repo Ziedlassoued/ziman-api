1dd68642ded1b364f0b2cc693e2fcfe2fe451824
“WebDevManuel” <donmanuel2005@web.de>
2021-10-29 10:46:37 +0200

    Add basic file and functions

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import { connectDatabase } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MONGODB_URI provided');
+}
 
 const app = express();
 const port = 3000;
+
+app.use((request, _response, next) => {
+  console.log('Request received', request.url);
+  next();
+});
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
@@ -10,3 +24,9 @@ app.get('/', (_req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
